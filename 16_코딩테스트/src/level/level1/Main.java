e71b727db25f1ce47877aef2c9d0b0f12b8a8dfb
bountyHB <gktqk1103@gmail.com>
2022-05-29 22:44:08 +0900

    220529

---
+++ 16_코딩테스트/src/level/level1/Main.java
@@ -0,0 +1,16 @@
+package level.level1;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int t = scanner.nextInt();
+		
+		for (int i = 0; i < args.length; i++) {
+			
+		}
+	}
+}

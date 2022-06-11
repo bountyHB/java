055aa4cd686c88f6d93ecca240c8db043f216288
bountyHB <gktqk1103@gmail.com>
2022-06-11 21:26:19 +0900

    no message

--- 16_코딩테스트/src/level/level1/Main.java
+++ 16_코딩테스트/src/level/level1/Main.java
@@ -1,23 +1,30 @@
 package level.level1;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
  
 public class Main {
  
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args){
  
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		StringBuilder sb = new StringBuilder();
- 
-		for (int i = 0; i < N; i++) {		
-			String str = br.readLine();
-			int target = str.indexOf(" ");
-			int result = Integer.parseInt(str.substring(0,target)) + Integer.parseInt(str.substring(target + 1));		
-			sb.append(result+"\n");
-		}
-		br.close();
-    }
+	       Scanner scanner = new Scanner(System.in);
+
+	       int num = scanner.nextInt();
+	       int[] intArr= new int[num];
+	       scanner.nextLine();
+	        
+	       String str = scanner.nextLine();
+	        // 띄어쓰기로 구분해서 나눔.
+	       String strArr[] = str.split(" ");
+
+	       for (int i = 0; i < num; i++) {
+	        // 나눈것을 형 변환해서 int배열에 넣음
+	        	intArr[i] = Integer.parseInt(strArr[i]);
+			}
+	        // 배열에서 min max로 최소값과 최대값 출력함.
+
+	       Arrays.sort(intArr);
+	       System.out.println(intArr[0]);
+	       System.out.println(intArr[num-1]);
+	    }
 }

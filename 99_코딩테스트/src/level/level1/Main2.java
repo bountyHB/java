f2a97924e2a19f5338474f189aec3b33d9dd1263
bountyHB <101177300+bountyHB@users.noreply.github.com>
2022-07-03 15:10:41 +0900

    Update Main2.java

--- 99_코딩테스트/src/level/level1/Main2.java
+++ 99_코딩테스트/src/level/level1/Main2.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class Main2 {
 
-    public static void main(String[] args){
+    public static void main(String[] args){ 
+        
         Scanner scanner = new Scanner(System.in);
         int[] arr = new int[9];
         int count = 0;
